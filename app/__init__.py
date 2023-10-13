973572457f4385d860bb12d5a27fdb279b7f5b77
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-10-13 01:05:16 -0600

    Conexión a la base de datos

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+
+mysql = MySQL(app)
