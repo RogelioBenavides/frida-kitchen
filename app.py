76a59de4e7bf756a38e0fd0d055557a222a46bf7
Ice-3 <joel.lopezp@udem.edu>
2023-10-14 19:25:49 -0400

    functioning/ingredients

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from app import app 
 
 # Import .py from routes
-from app.routes import route, truck, sample, identity, identity_company, long_stop, company, sampling, uploaded_file, meals
+from app.routes import route, truck, sample, identity, identity_company, long_stop, company, sampling, uploaded_file, meals, ingredients
 
 if __name__ == "__main__":
     app.run(debug = True)
