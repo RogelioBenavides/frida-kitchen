4707300e86dd471bd449ef0e49ea845bbeb1409e
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-10-14 02:26:13 -0600

    json y xml de identity_company

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from app import app
 from app.routes.route import *
 from app.routes.truck import *
 from app.routes.sample import *
+from app.routes.identity_company import *
 
 if __name__ == "__main__":
     app.run(debug = True)
