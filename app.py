40e651c87009696782d757f431475861178ae4ac
Ice-3 <joel.lopezp@udem.edu>
2023-10-14 17:34:53 -0400

    functioning/sampling

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app.routes.route import *
 from app.routes.truck import *
 from app.routes.identity import *
 from app.routes.identity_company import *
+from app.routes.sampling import *
 
 if __name__ == "__main__":
     app.run(debug = True)
