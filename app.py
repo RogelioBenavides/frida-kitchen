5cdc860392668b31d1e2ffbdf9b7011e5a977522
Ice-3 <joel.lopezp@udem.edu>
2023-10-14 17:17:12 -0400

    company funcionando

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from app import app
+from app import app 
 
 from app.routes.route import *
 from app.routes.truck import *
+from app.routes.sample import *
 from app.routes.identity import *
 from app.routes.identity_company import *
+from app.routes.company import *
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ app/routes/sample.py
@@ -0,0 +1,49 @@
+from app import app, mysql
+from flask import Flask, render_template, request, jsonify, make_response, redirect, url_for
+import xml.etree.ElementTree as ET
+
+@app.route('/muestras', methods=['GET','POST'])
+def get_sample():
+    if request.method == 'POST':
+        id = request.form.get('sample_id')
+        format = request.form.get('format')
+
+        return redirect(url_for('show_sample', id = id, format = format))
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT truck_name, id FROM sample')
+    samples = cursor.fetchall()
+    cursor.close()
+
+    return render_template('samples.html', samples = samples)
+
+@app.route('/muestra/<int:id>&<format>', methods=['GET'])
+def show_sample(id, format):
+    cursor = mysql.connection.cursor()
+    cursor.execute('SELECT * FROM sample where id = %s', (id,))
+    row = cursor.fetchone()
+    cursor.close()
+
+    if not row:
+        return jsonify({"mensaje":"Muestra no encontrada"}), 404
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute('DESCRIBE sample')
+    columns = [column[0] for column in cursor.fetchall()]
+    cursor.close()
+
+    sample_dataJSON = dict(zip(columns, row))
+
+    if format == 'json':
+        return jsonify(sample_dataJSON)
+    elif format == 'xml':
+        sample_dataXML = ET.Element('sample')
+
+        for column, value in zip(columns, row):
+            ET.SubElement(sample_dataXML, column).text = str(value)
+        
+        response = make_response(ET.tostring(sample_dataXML))
+        response.headers['Content-Type'] = 'application/xml'
+        return response
+    else:
+        return jsonify({"mensaje":"Formato invalido"})
