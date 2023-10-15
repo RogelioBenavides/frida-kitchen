873553860aadf31e6fb1264bdeb840ae3a979f10
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-10-14 18:12:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from app import app
 
 # Import .py from routes
 from app.routes import route, truck, sample, identity, identity_company, long_stop, company, sampling
+
 if __name__ == "__main__":
     app.run(debug = True)
