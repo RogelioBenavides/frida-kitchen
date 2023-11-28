ae85f0fa5a27f9a55f8c2b1cf1e035345431d0b2
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2023-11-28 01:44:18 -0600

    Edit meal

--- backend/flask_app/meals.py
+++ backend/flask_app/meals.py
@@ -86,13 +86,54 @@ def add_meal():
 def edit_meal(id):
     meal_name = request.form['meal_name']
     price = request.form['price']
+    description = request.form['description']
 
-    if meal_name and price:
+    if not request.files['image']:
+        if meal_name and price and description:
+            cursor = mysql.connection.cursor()
+            cursor.execute("UPDATE meals SET meal_name = %s, price = %s, description = %s WHERE id = %s", (meal_name, price, description, id))
+            mysql.connection.commit()
+            cursor.close()
+            return redirect('/meals')
+        
+    file = request.files['image']
+
+    # Checking if the image is valid
+    try:
+        img = Image.open(file.stream)
+        img.verify()
+    except Exception as e:
+        print(e)
+        return redirect('/meals') # Leave with no changes
+
+    # Upload Image to IMGGB
+    api_url = "https://api.imgbb.com/1/upload"
+
+    img = Image.open(file.stream)
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="JPEG")
+    img_bytes = img_bytes.getvalue()
+
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+    params = {"key":os.environ.get('IMGBB_KEY'),
+              "image": img_base64}
+    
+    response = requests.post(api_url, params)
+    response = json.loads(response.text)
+
+    saved_img_url = response["data"]["url"]
+
+    if meal_name and price and description and img and saved_img_url:
         cursor = mysql.connection.cursor()
-        cursor.execute("UPDATE meals SET meal_name = %s, price = %s WHERE id = %s", (meal_name, price, id))
+        cursor.execute("UPDATE meals SET meal_name = %s, price = %s, description = %s, image_url = %s WHERE id = %s", (meal_name, price, description, saved_img_url, id))
         mysql.connection.commit()
         cursor.close()
+
     return redirect('/meals')
+        
+    
 
 @app.route('/meals/delete', methods=['GET'])
 def delete_meal():
