387cdcda47a14ee496c01544eb07cfdba43322d5
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2023-11-29 02:32:55 -0600

    Login method

--- backend/flask_app/login.py
+++ backend/flask_app/login.py
@@ -5,3 +5,28 @@ from flask_jwt_extended import create_access_token
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+@app.route("/pythonlogin", methods=['GET', 'POST'])
+def pythonlogin():
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        #Extract Credentials
+        _user = request.form['username']
+        _password = request.form['password']
+
+        #Validate in MySQL
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM users WHERE email = %s AND user_password = %s;', (_user, _password))
+        account = cursor.fetchone()
+        cursor.close()
+
+        if not account:
+            return render_template('login.html', msg='Incorrect username or password')
+        
+        if account[5] != 'admin':
+            return render_template('login.html', msg='Your user is not an administrator')
+        
+        session['loggedin'] = True
+        session['email'] = account[3]
+
+        return redirect('/meals')
+
