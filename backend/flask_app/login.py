50493055ff1872f59f67f3c52acd47e9ff9b7c86
Carlos Lozada <36938844+celr00@users.noreply.github.com>
2023-11-29 02:47:01 -0600

    Removed unused imports

--- backend/flask_app/login.py
+++ backend/flask_app/login.py
@@ -1,6 +1,5 @@
-from flask_app import app, mysql, jwt
+from flask_app import app, mysql
 from flask import Flask, request, render_template, session, redirect
-from flask_jwt_extended import create_access_token
 
 @app.route("/login")
 def login():
