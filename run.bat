9c8444b98655ee903b1f1f105d3637992c6456f7
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-11-29 23:15:49 -0600

    update run.bat

--- run.bat
+++ run.bat
@@ -3,24 +3,24 @@ echo Entrando al entorno virtual
 call .venv\Scripts\activate
 timeout /nobreak /t 2 >nul
 
-echo Ejecutando microservicio backend en el puerto 5000
+echo Ejecutando microservicio backend
 start "Backend" cmd /c python backend\app.py
 timeout /nobreak /t 2 >nul
 
-echo Ejecutando microservicio Inicio y Registro en el puerto 5001
+echo Ejecutando microservicio Inicio y Registro
 start "AuthService" cmd /c python auth_service\app.py
 timeout /nobreak /t 2 >nul
 
-echo Ejecutando microservicio Ordenes en el puerto 5002
+echo Ejecutando microservicio Ordenes
 start "OrdersService" cmd /c python orders_service\app.py
 timeout /nobreak /t 2 >nul
 
-echo Ejecutando microservicio Tracking en el puerto 5004
+echo Ejecutando microservicio Tracking
 start "TrackingService" cmd /c python tracking_service\app.py
 timeout /nobreak /t 2 >nul
 
-echo Ejecutando microservicio Comidas en el puerto 5006
-start "MealService" cmd /c python tracking_service\app.py
+echo Ejecutando microservicio Comidas
+start "MealService" cmd /c python meals_service\app.py
 timeout /nobreak /t 2 >nul
 
 echo Saliendo del entorno virtual
